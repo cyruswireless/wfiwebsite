4aa537039c6fb3fef85c4e2a00449c5896f07a52
Mónica Sánchez <mk_sanmor@hotmail.com>
2022-07-26 14:46:35 -0500

    Se traducen mas informacion

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -12,14 +12,29 @@ export class HeaderComponent implements OnInit {
     public translate: TranslateService
   ) {
     translate.addLangs(['en']);
-    translate.setDefaultLang('es');
-    translate.use('es');
   }
 
   ngOnInit(): void {
+    let userLang = navigator.language ? navigator.language.substring(0, 2) : 'es';
+    this.translate.use(userLang);
+    this.setFlag(userLang);
   }
 
   changeLang(lang: string) {
     this.translate.use(lang);
+    this.setFlag(lang);
+  }
+
+  setFlag(lang: string): void {
+    let flagMx = document.getElementById('flagMx');
+    let flagEn = document.getElementById('flagEn');
+  
+    if(lang == 'es') {
+      flagMx ? flagMx.style.filter = "none" : null;
+      flagEn ? flagEn.style.filter = "brightness(0.5)" : null;
+    } else if (lang == 'en') {
+      flagEn ? flagEn.style.filter = "none" : null;
+      flagMx ? flagMx.style.filter = "brightness(0.5)" : null;
+    }
   }
 }
