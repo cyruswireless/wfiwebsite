2de8017e6ea0933b7b368ec909361917675d449c
Mónica Sánchez <mk_sanmor@hotmail.com>
2022-07-26 18:25:25 -0500

    Se separa el slide del about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MainSectionComponent } from './components/home/main-section/main-section.component';
-import { HeaderComponent } from './components/shared/header/header.component';
+import { AboutComponent } from './components/home/about/about.component';
+import { SlideComponent } from './components/home/slide/slide.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainSectionComponent,
-    FooterComponent
+    AboutComponent,
+    FooterComponent,
+    SlideComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/home/about/about.component.spec.ts
+++ src/app/components/home/about/about.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainSectionComponent } from './main-section.component';
+import { AboutComponent } from './about.component';
 
 describe('MainSectionComponent', () => {
-  let component: MainSectionComponent;
-  let fixture: ComponentFixture<MainSectionComponent>;
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MainSectionComponent ]
+      declarations: [ AboutComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MainSectionComponent);
+    fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
