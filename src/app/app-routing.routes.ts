5f84071e9844fc915d4681e5986e37a3ca1f1abf
Mónica Sánchez <mk_sanmor@hotmail.com>
2022-07-29 12:17:13 -0500

    Se agrega el caso de exito de veracruz

--- src/app/app-routing.routes.ts
+++ src/app/app-routing.routes.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { StoriesComponent } from './components/stories/stories.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/home' },
     { path: 'home', component: LandingComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'about/stories', component: StoriesComponent },
     { path: '**', pathMatch: 'full', redirectTo: '/home' }
 ];
 
