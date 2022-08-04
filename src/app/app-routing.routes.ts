40a74664c3ec3c8586eaec76af46720228d38684
Jose Montiel <josemontiel@me.com>
2022-08-03 23:06:12 -0500

    feat: Se agregan rutas de servicios y nosotros, se modifica novbar para contemplarlo

--- src/app/app-routing.routes.ts
+++ src/app/app-routing.routes.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './components/sections/about/about.component';
+import { AboutComponent } from './components/about/about.component';
 import { AwardsComponent } from './components/sections/awards/awards.component';
 import { OpticalFiberComponent } from './components/sections/optical-fiber/optical-fiber.component';
 import { StoriesComponent } from './components/sections/stories/stories.component';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { ServicesComponent } from './components/services/services.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/home' },
     { path: 'home', component: HomeComponent },
     { path: 'experience', component: ExperienceComponent },
-    { path: 'contact', component: ContactComponent },
+    { path: 'services', component: ServicesComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
     { path: 'about/stories', component: StoriesComponent },
     { path: 'about/awards', component: AwardsComponent },
     { path: 'experience/op-fiber', component: OpticalFiberComponent },
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about-route',
+  selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
