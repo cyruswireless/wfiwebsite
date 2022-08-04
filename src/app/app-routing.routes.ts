3fa18394c7dd8cd66dbe5d0c7def81649bdcb48b
Mónica Sánchez <mk_sanmor@hotmail.com>
2022-08-04 13:32:54 -0500

    REFACTOR: Colocando el componente dentro de la carpeta de seccionees

--- src/app/app-routing.routes.ts
+++ src/app/app-routing.routes.ts
@@ -4,9 +4,9 @@ import { AboutComponentRoute } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { HomeComponent } from './components/home/home.component';
-import { PrivacyNoticeComponent } from './components/privacy-notice/privacy-notice.component';
 import { AwardsComponent } from './components/sections/awards/awards.component';
 import { OpticalFiberComponent } from './components/sections/optical-fiber/optical-fiber.component';
+import { PrivacyNoticeComponent } from './components/sections/privacy-notice/privacy-notice.component';
 import { StoriesComponent } from './components/sections/stories/stories.component';
 import { ServicesComponent } from './components/services/services.component';
 
