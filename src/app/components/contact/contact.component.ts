70fecb2eeaff9cdd0360af98d2645a7467b0b029
Mónica Sánchez <mk_sanmor@hotmail.com>
2022-08-12 14:19:45 -0500

    Se conecta el formulario con la api y se crean validacioines para el form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,14 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  recordForm!: FormGroup;
+  alert?: boolean = false;
 
-  constructor() { }
+  Record = {
+    name: '',
+    enterprise: '',
+    email: '',
+    subject: ''
+  };
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.recordForm = new FormGroup({
+      'name': new FormControl(this.Record.name, [Validators.required, Validators.minLength(8), Validators.maxLength(100)]),
+      'enterprise': new FormControl(this.Record.enterprise, [Validators.required, Validators.minLength(4), Validators.maxLength(100)]),
+      'email': new FormControl(this.Record.email, [Validators.required, Validators.maxLength(50), Validators.pattern("[^ @]*@[^ @]*")]),
+      'subject': new FormControl(this.Record.subject, [Validators.required, Validators.minLength(10), Validators.maxLength(600)])
+    });
   }
 
-  enviar(){
-    alert("Lo sentimos, sevicio no disponible");
+  get name() { return this.recordForm.get('name'); }
+  get enterprise() { return this.recordForm.get('enterprise'); }
+  get email() { return this.recordForm.get('email'); }
+  get subject() { return this.recordForm.get('subject'); }
+
+  sendRecord(){
+    let url = `http://localhost:81/record`;
+
+    let data = {
+      name: this.recordForm.value.name,
+      enterprise: this.recordForm.value.enterprise,
+      email: this.recordForm.value.email,
+      subject: this.recordForm.value.subject
+    }
+
+    this.http.post(url, data).subscribe(
+      (res:any) => {
+        if(res.status == 1) {
+          this.alert = true;
+          this.recordForm.reset();
+        }
+      }
+    );
   }
 
+  closeAlert () {
+    this.alert = false;
+  }
 }
